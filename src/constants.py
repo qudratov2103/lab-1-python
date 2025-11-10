85f67f8af2b7d4ef714b873519731a4fb444f985
qudratov2103 <nkkudratov@mai.education>
2025-11-10 10:09:12 +0300

    filled constants module with actual values

---
+++ src/constants.py
@@ -0,0 +1,10 @@
+VAL = {
+    '+': lambda x, y: x + y,
+    '-': lambda x, y: x - y,
+    '*': lambda x, y: x * y,
+    '/': lambda x, y: x / y,
+    '**': lambda x, y: x ** y
+}
+
+ORDER = {'+': 1, '-': 1, '*': 2, '/': 2, '**': 3}
+# EXAMPLE = "ПРИМЕР" 
